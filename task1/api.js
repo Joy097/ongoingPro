6f1aad4c6025fc7ee1be824cad8e0af73bdb49bc
Joy097 <69787432+Joy097@users.noreply.github.com>
2023-01-30 19:56:27 +0600

    Update api.js

--- task1/api.js
+++ task1/api.js
@@ -1,34 +1,43 @@
 
-function show(){
+async function show(){
     const id = document.getElementById("id").value.trim()
     const name = document.getElementById("nam").value.trim()
-console.log(id)
-    fetch(`https://jsonplaceholder.typicode.com/${name}/${id}`)
-            .then(response => response.json())
-            .then(json => {
-                var result = document.getElementById('result');
-                let keys = Object.keys(json);
-                result.innerHTML = `${keys[0]} : `+json[keys[0]]+"<br>"+
-                `${keys[1]} : ` +json[keys[1]]+"<br>"+
-                `${keys[2]} : ` +json[keys[2]]+"<br>"+
-                `${keys[3]} : ` + json[keys[3]]
-            })
+    let result = document.getElementById('result');
+
+    let url = `https://jsonplaceholder.typicode.com/${name}/${id}`
+    let b = await fetcher(url)
+    console.log(b)
+
+    result.innerHTML = add(b)
 }
-function showall(){
+async function showall(){
     const name = document.getElementById("name").value.trim()
+    let result = document.getElementById('result');
     let a =''
-    fetch(`https://jsonplaceholder.typicode.com/${name}`)
-    .then(response => response.json())
-    .then(json => {
-        let result = document.getElementById('result');
-        json.forEach(element=>{
-            let keys = Object.keys(element)
-            let b = `${keys[0]} : `+element[keys[0]]+"<br>"+
-            `${keys[1]} : ` +element[keys[1]]+"<br>"+
-            `${keys[2]} : ` +element[keys[2]]+"<br>"+
-            `${keys[3]} : ` + element[keys[3]]  +"<br><br>"
-        a = a.concat(b)
-        })
-        result.innerHTML = a
-    })
+    let url = `https://jsonplaceholder.typicode.com/${name}`
+    let b = await fetcher(url)
+    b.forEach(element => {
+    let d = add(element)
+    a = a.concat(d)})
+    result.innerHTML = a
+}
+
+async function fetcher(url){
+    
+    let v = await fetch(url)
+    let c = await v.json()
+    return  c
+    
+}
+
+function add(element){
+
+        let keys = Object.keys(element)
+        let x = `${keys[0]} : `+element[keys[0]]+"<br>"+
+        `${keys[1]} : ` +element[keys[1]]+"<br>"+
+        `${keys[2]} : ` +element[keys[2]]+"<br>"+
+        `${keys[3]} : ` + element[keys[3]]  +"<br><br>"
+        return x;
+    
+   
 }
