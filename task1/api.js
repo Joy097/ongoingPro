6f25bc67478ffef4551808d7cdbaeaea822ee41f
Joy097 <69787432+Joy097@users.noreply.github.com>
2023-01-31 11:47:23 +0600

    Update api.js

--- task1/api.js
+++ task1/api.js
@@ -41,3 +41,60 @@ function add(element){
     
    
 }
+
+function post(){
+    const name = document.getElementById("nam1").value.trim()
+    const id = document.getElementById("nam2").value.trim()
+    const body = document.getElementById("nam3").value.trim()
+    let result = document.getElementById('result');
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+    title: name,
+    body: id,
+    userId: body,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => result.innerHTML= add(json));
+}
+
+function update(){
+    const id = document.getElementById("id1").value.trim()
+    const title = document.getElementById("ttl").value.trim()
+    const body = document.getElementById("bdy").value.trim()
+    const userid = document.getElementById("uid").value.trim()
+
+    let result = document.getElementById('result');
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: id,
+    title: title,
+    body: body,
+    userId: userid,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => result.innerHTML= add(json));
+}
+
+function del(){
+    const id = document.getElementById("id").value.trim()
+    const name = document.getElementById("nam").value.trim()
+    let result = document.getElementById('result');
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'DELETE',
+});
+    .then((response) => (response.json()))
+    .then((json) => result.innerHTML= add(json));
+}
